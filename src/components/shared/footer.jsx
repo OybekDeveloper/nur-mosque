0df2dd0390902eb7371e9450d4f20fad0256a59e
OybekDeveloper <programmeroybek211@gmail.com>
2025-07-23 11:23:19 +0500

    first commit for site

--- src/components/shared/footer.jsx
+++ src/components/shared/footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='w-screen flex justify-center items-center bg-gray-100 dark:bg-gray-800'>
-      <main className='text-[#638773] max-w-[1440px] mx-auto w-11/12 py-6 flex justify-center items-center flex-col gap-4'>
+    <footer className='text-[#638773] w-screen flex justify-center items-center bg-gray-100 dark:bg-gray-800'>
+      <main className='max-w-[1440px] mx-auto w-11/12 py-6 flex justify-center items-center flex-col gap-4'>
         <ul className='flex w-full justify-around items-center gap-4'>
           <li className='inline-block mr-4'>
             <a href="/">About us</a>
@@ -19,10 +19,15 @@ export default function Footer() {
             <a href="/privacy">Terms of Service</a>
           </li>
         </ul>
-        <div cla>
+        <div className='flex justify-center items-center gap-4'>
           <Image src="/icons/facebook.svg" alt="Twitter Icon" width={24} height={24} className="inline-block mr-2" />
           <Image src="/icons/twitter.svg" alt="Twitter Icon" width={24} height={24} className="inline-block mr-2" />
           <Image src="/icons/instagram.svg" alt="Twitter Icon" width={24} height={24} className="inline-block mr-2" />
+        </div>
+        <div>
+          <p className='text-sm'>
+            © {new Date().getFullYear()} Nur Mosque. All rights reserved.
+          </p>
         </div>
       </main>
     </footer>
